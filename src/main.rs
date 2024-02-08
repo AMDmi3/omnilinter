377f4d71f92966bb7fc321a4a654b7b21e6f5717
Dmitry Marakasov <amdmi3@amdmi3.ru>
2024-02-08 21:07:28 +0300

    Add command line args parser (fixes #2)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod parser;
 mod ruleset;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to configuration file
+    #[arg(short = 'c', long, value_name = "CONFIG_PATH")]
+    config: String,
+
+    /// Paths to directories to operate on
+    #[arg(value_name = "TARGET_DIR")]
+    targets: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
