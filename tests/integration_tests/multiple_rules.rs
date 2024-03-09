AMDmi3/omnilinter

commit 5be5a609e995da1251352ada1a349bc8e250ca8c

Fix content matching bug
Because matched lines were added to rule status before all conditions
were known to be satisfied, false matches could be reported, ignoring
preconditions.

src/applier.rs
@@ -26,13 +26,14 @@ fn apply_content_rules(
     let reader = BufReader::new(file);
 
     let mut local_condition_statuses: Vec<bool> = vec![false; global_condition_statuses.len()];
+    let mut matched_lines: Vec<usize> = vec![];
 
     for (line_number, line) in reader.lines().enumerate() {
         let line = line?;
 
         let mut matching_cache = RegexMatchingCache::new(&line, ruleset.regexes_count);
 
-        rules_with_conditions.retain(|(rule, path_condition)| {
+        rules_with_conditions.retain(|(_, path_condition)| {
             let mut num_satisfied_content_conditions = 0;
             for content_condition in &path_condition.content_conditions {
                 match content_condition.logic {
@@ -46,9 +47,7 @@ fn apply_content_rules(
                         if content_condition.is_reporting_target {
                             if matching_cache.check_condition_match(content_condition) {
                                 *is_matched = true;
-                                global_rule_statuses[rule.number]
-                                    .matched_lines
-                                    .push((path.clone(), line_number));
+                                matched_lines.push(line_number);
                             }
                         } else if !*is_matched {
                             *is_matched = matching_cache.check_condition_match(content_condition);
@@ -75,6 +74,11 @@ fn apply_content_rules(
         }
 
         global_condition_statuses[condition.number] = true;
+        global_rule_statuses[rule.number].matched_lines.extend(
+            matched_lines
+                .iter()
+                .map(|line_number| (path.clone(), *line_number)),
+        );
 
         if condition.is_reporting_target {
             global_rule_statuses[rule.number]

tests/integration_tests/content_condition_combinations.rs
@@ -129,3 +129,14 @@ fn nomatch_after_match_d() {
         .run()
         .assert_matches(vec![]);
 }
+
+#[test]
+fn multiple_matches_in_same_file() {
+    TestCase::new_for_json_tests()
+        .add_file("a", lines!["b", "a"])
+        .add_file("b", lines!["a", "b"])
+        .add_file("c", lines!["b"])
+        .add_rule(lines!["files *", "match /^a/", "match /^b/"])
+        .run()
+        .assert_matches(vec!["a:1", "b:2"]); // not c:1!!! because it does not have "a" match
+}

commit e32d1ba7cae48bf6fb5ccb963beeeeb2051a7896

Fix regression introduced in 5be5a60
Line matches should belong to a specific rule

src/applier.rs
@@ -26,14 +26,14 @@ fn apply_content_rules(
     let reader = BufReader::new(file);
 
     let mut local_condition_statuses: Vec<bool> = vec![false; global_condition_statuses.len()];
-    let mut matched_lines: Vec<usize> = vec![];
+    let mut matched_lines: Vec<Vec<usize>> = vec![Default::default(); global_rule_statuses.len()];
 
     for (line_number, line) in reader.lines().enumerate() {
         let line = line?;
 
         let mut matching_cache = RegexMatchingCache::new(&line, ruleset.regexes_count);
 
-        rules_with_conditions.retain(|(_, path_condition)| {
+        rules_with_conditions.retain(|(rule, path_condition)| {
             let mut num_satisfied_content_conditions = 0;
             for content_condition in &path_condition.content_conditions {
                 match content_condition.logic {
@@ -47,7 +47,7 @@ fn apply_content_rules(
                         if content_condition.is_reporting_target {
                             if matching_cache.check_condition_match(content_condition) {
                                 *is_matched = true;
-                                matched_lines.push(line_number);
+                                matched_lines[rule.number].push(line_number);
                             }
                         } else if !*is_matched {
                             *is_matched = matching_cache.check_condition_match(content_condition);
@@ -75,7 +75,7 @@ fn apply_content_rules(
 
         global_condition_statuses[condition.number] = true;
         global_rule_statuses[rule.number].matched_lines.extend(
-            matched_lines
+            matched_lines[rule.number]
                 .iter()
                 .map(|line_number| (path.clone(), *line_number)),
         );

tests/integration_tests/mod.rs
@@ -8,6 +8,7 @@ mod error_exitcode;
 mod glob_patterns;
 mod glob_scope;
 mod ignore_marker;
+mod multiple_rules;
 mod path_condition_combinations;
 mod path_conditions;
 mod premature;

tests/integration_tests/multiple_rules.rs
@@ -1,0 +1,17 @@
+// SPDX-FileCopyrightText: Copyright 2024 Dmitry Marakasov <amdmi3@amdmi3.ru>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use crate::lines;
+use crate::utils::TestCase;
+
+#[test]
+fn mixing_up_matches_bug() {
+    // test for actual bug where rules produced false matches from another rules
+    // which produced vec!["a:1", "a:1", "a:2", "a:2"] matches
+    TestCase::new_for_json_tests()
+        .add_file("a", lines!["a", "b"])
+        .add_rule(lines!["files *", "match /a/"])
+        .add_rule(lines!["files *", "match /b/"])
+        .run()
+        .assert_matches(vec!["a:1", "a:2"]);
+}